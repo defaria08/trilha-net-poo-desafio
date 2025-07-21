e57fa2fb8f7a9ac267d7c5f874f575fbad506690
defaria08 <difaria08@gmail.com>
2025-07-21 14:16:18 -0300

    Alteracao desafio DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smart Nokia");
+Smartphone Nokia = new Nokia(numero: "12982108744", modelo: "A1200", imei: "23ODFUOSF932", memoria: 32);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smart Nokia");
+Smartphone Iphone = new Nokia(numero: "12996548888", modelo: "S23FE", imei: "123IAIDSJI12", memoria: 128);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Telegram");
